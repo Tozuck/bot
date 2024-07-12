1c44627e2d5c4e31dcb951ed93fb9c198eb27144
Tozuck <122993981+Tozuck@users.noreply.github.com>
2024-07-13 03:08:27 +0330

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 # Define the necessary variables
-BOT_SCRIPT="~/bot/toz.py"
-REQUIREMENTS_FILE="~/bot/requirements.txt"
-LOG_DIR="logs"
-VENV_DIR="venv"
-CRON_JOB="@reboot ~/bot/bot.sh"
+BOT_SCRIPT="toz.py"
+REQUIREMENTS_FILE="requirements.txt"
+LOG_DIR="$(pwd)/logs"
+VENV_DIR="$(pwd)/venv"
+CRON_JOB="@reboot $(pwd)/setup_and_run_bot.sh"
 
 # Update package list and install system dependencies
 echo "Updating package list and installing system dependencies..."
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-dev libzbar0
-
-# Install virtualenv if it's not installed
-echo "Installing virtualenv..."
-pip3 install virtualenv
-
+cd bot
 # Create a virtual environment
 echo "Creating a virtual environment..."
-virtualenv $VENV_DIR
+python3 -m venv $VENV_DIR
 
 # Activate the virtual environment
 echo "Activating the virtual environment..."
@@ -47,6 +43,6 @@ nohup python3 $BOT_SCRIPT > $LOG_DIR/bot.log 2>&1 &
 
 # Add crontab entry to ensure the bot starts on reboot
 echo "Adding crontab entry..."
-(crontab -l ; echo "$CRON_JOB") | crontab -
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
 
 echo "Bot setup complete and running in the background."

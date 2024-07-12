730fde9824431c9ab5b534b61ac7c11366d340b9
Tozuck <122993981+Tozuck@users.noreply.github.com>
2024-07-13 03:13:21 +0330

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Define the necessary variables
-BOT_SCRIPT="toz.py"
-REQUIREMENTS_FILE="requirements.txt"
-LOG_DIR="$(pwd)/logs"
-VENV_DIR="$(pwd)/venv"
-CRON_JOB="@reboot $(pwd)/setup_and_run_bot.sh"
+BOT_DIR="$HOME/bot"
+BOT_SCRIPT="$BOT_DIR/toz.py"
+REQUIREMENTS_FILE="$BOT_DIR/requirements.txt"
+LOG_DIR="$BOT_DIR/logs"
+VENV_DIR="$BOT_DIR/venv"
+PYTHON="$VENV_DIR/bin/python"
+PIP="$VENV_DIR/bin/pip"
+CRON_JOB="@reboot $BOT_DIR/bot.sh"
 
 # Update package list and install system dependencies
 echo "Updating package list and installing system dependencies..."
 sudo apt-get update
 sudo apt-get install -y python3-pip python3-dev libzbar0
-cd bot
+
 # Create a virtual environment
 echo "Creating a virtual environment..."
 python3 -m venv $VENV_DIR
@@ -20,18 +23,9 @@ python3 -m venv $VENV_DIR
 echo "Activating the virtual environment..."
 source $VENV_DIR/bin/activate
 
-# Create requirements.txt
-echo "Creating requirements.txt..."
-cat > $REQUIREMENTS_FILE << EOL
-pyTelegramBotAPI==4.9.1
-requests==2.28.1
-Pillow==9.1.1
-pyzbar==0.1.9
-EOL
-
 # Install Python dependencies
 echo "Installing Python dependencies..."
-pip install -r $REQUIREMENTS_FILE
+$PIP install -r $REQUIREMENTS_FILE
 
 # Create a log directory if it doesn't exist
 echo "Creating log directory..."
@@ -39,7 +33,7 @@ mkdir -p $LOG_DIR
 
 # Run the bot in the background
 echo "Running the bot in the background..."
-nohup python3 $BOT_SCRIPT > $LOG_DIR/bot.log 2>&1 &
+nohup $PYTHON $BOT_SCRIPT > $LOG_DIR/bot.log 2>&1 &
 
 # Add crontab entry to ensure the bot starts on reboot
 echo "Adding crontab entry..."
